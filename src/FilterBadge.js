67ce32e244b63bf8dbf17faadd3a942d3a0605e4
Marc Saleiko <marc.saleiko@komadesign.de>
2020-12-14 08:19:46 +0100

    [FEATURE] Adds FilterBadge "all" badge

--- src/FilterBadge.js
+++ src/FilterBadge.js
@@ -4,60 +4,97 @@
  */
 (function ($) {
 
-    $.fn.FilterBadge = function (options) {
-      var settings = $.extend({
-        activeClass: 'filter-badge--active',
-      }, options)
-  
-      var onBadgeClick = function (e) {
-        var $this = $(this)
-  
-        if (typeof Filter !== 'undefined' && Filter.isActive() === true)
+  $.fn.FilterBadge = function (options) {
+    var settings = $.extend({
+      activeClass: 'filter-badge--active',
+      // How do we access all other filter badges?
+      // If multiple is set to false, which selector should we use to
+      // deselect all active badges
+      // We also use this selector to deselect active badges when a badge
+      // with the keyword "all" was clicked to active or if
+      // all other badges are deselected and all should be active
+      baseSelector: '.js-filter-badge',
+      allBadgeSelector: '.js-filter-badge-all',
+    }, options)
+
+    var onBadgeClick = function (e) {
+      var $this = $(this)
+
+      if (typeof Filter !== 'undefined' && Filter.isActive() === true)
+      {
+        if ($this.data('active') === true)
         {
-          if ($this.data('active') === true)
-          {
-            this.filterValue.lastValue = ''
-            $this.removeClass(settings.activeClass)
-            $this.data('active', false)
-          } else
-          {
-            this.filterValue.lastValue = $this.data('value')
-            $this.addClass(settings.activeClass)
-            $this.data('active', true)
+          this.filterValue.lastValue = ''
+          $this.removeClass(settings.activeClass)
+          $this.data('active', false)
+          // is any active? if not, activate "all" badge
+          if( $('.'+settings.activeClass).length === 0) {
+            activateAllBadge()
           }
-  
-          Filter.update()
+        } else
+        {
+          this.filterValue.lastValue = $this.data('value')
+          // use the keyword "all" to disable filtering
+          if( this.filterValue.lastValue === 'all' ) {
+            this.filterValue.lastValue = ''
+            deactivateAllOtherBadges()
+          } else {
+            deactivateAllBadge()
+          }
+          $this.addClass(settings.activeClass)
+          $this.data('active', true)
+        }
+
+        Filter.update()
+      }
+    }
+
+    var activateAllBadge = function() {
+      $(settings.allBadgeSelector).addClass(settings.activeClass).data('active', true)
+    }
+
+    var deactivateAllBadge = function() {
+      $(settings.allBadgeSelector).removeClass(settings.activeClass).data('active', false)
+    }
+
+    var deactivateAllOtherBadges = function() {
+      $('.'+settings.activeClass).not(settings.allBadgeSelector).each( function() {
+        $(this).removeClass(settings.activeClass).data('active', false)
+        this.filterValue.lastValue = ''
+      })
+    }
+
+    return this.each(function () {
+      var $this = $(this)
+
+      settings = $.extend(settings, $this.data())
+
+      if (typeof Filter !== 'undefined')
+      {
+        if (typeof $this.data('value') === 'undefined')
+        {
+          $this.data('value', '')
+        }
+
+        $this.data('active', false)
+
+        this.filterValue = {
+          dimensionType: 'badge',
+          direction: '',
+          operator: 'or',
+          lastValue: '',
+          attribute: 'class'
         }
       }
-  
-      return this.each(function () {
-        var $this = $(this)
-  
-        settings = $.extend(settings, $this.data())
-  
-        if (typeof Filter !== 'undefined')
-        {
-          var initialValue = ''
-          if (typeof $this.data('value') === 'undefined')
-          {
-            $this.data('value', '')
-          } else
-  
-          $this.data('active', false)
-  
-          this.filterValue = {
-            dimensionType: 'badge',
-            direction: '',
-            operator: 'or',
-            lastValue: initialValue,
-            attribute: 'class'
-          }
-        }
-  
-        $this.on('click', onBadgeClick)
-  
-        return $this
-      })
-    };
-  
-  })(jQuery);
+
+      $this.on('click', onBadgeClick)
+
+      // if this is the "all" badge, activate it by default
+      if( $this.filter(settings.allBadgeSelector).length > 0 ) {
+        $this.addClass(settings.activeClass).data('active', true)
+      }
+
+      return $this
+    })
+  };
+})(jQuery);
