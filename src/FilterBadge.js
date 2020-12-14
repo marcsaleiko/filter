25d4b65e755999e92030d16a5a64658749b5aa08
Marc Saleiko <marc.saleiko@komadesign.de>
2020-12-14 08:34:11 +0100

    [FEATURE] Adds support for multiple and single selection

--- src/FilterBadge.js
+++ src/FilterBadge.js
@@ -1,12 +1,16 @@
 /**
  * FilterBadge
- *
  */
 (function ($) {
 
   $.fn.FilterBadge = function (options) {
     var settings = $.extend({
+      // the class we use to mark a badge as active. You may use this
+      // for styling purposes. please keep in mind, that we also
+      // use this class to select active badges
       activeClass: 'filter-badge--active',
+      // allow multiple badges to be active
+      multiple: true,
       // How do we access all other filter badges?
       // If multiple is set to false, which selector should we use to
       // deselect all active badges
@@ -14,6 +18,8 @@
       // with the keyword "all" was clicked to active or if
       // all other badges are deselected and all should be active
       baseSelector: '.js-filter-badge',
+      // If you add a badge that should show all entries, use this
+      // selector to make it reachable by this script or override it via the options
       allBadgeSelector: '.js-filter-badge-all',
     }, options)
 
@@ -27,20 +33,34 @@
           this.filterValue.lastValue = ''
           $this.removeClass(settings.activeClass)
           $this.data('active', false)
+
           // is any active? if not, activate "all" badge
           if( $('.'+settings.activeClass).length === 0) {
             activateAllBadge()
           }
-        } else
-        {
+
+        } else {
+
           this.filterValue.lastValue = $this.data('value')
+
           // use the keyword "all" to disable filtering
+          var allBadgeClicked = false
           if( this.filterValue.lastValue === 'all' ) {
             this.filterValue.lastValue = ''
-            deactivateAllOtherBadges()
-          } else {
-            deactivateAllBadge()
+            allBadgeClicked = true
           }
+
+          if( settings.multiple ) {
+            if( allBadgeClicked ) {
+              deactivateAllOtherBadges()
+            } else {
+              deactivateAllBadge()
+            }
+          }
+          else {
+            deactivateAllBadges()
+          }
+
           $this.addClass(settings.activeClass)
           $this.data('active', true)
         }
@@ -59,6 +79,13 @@
 
     var deactivateAllOtherBadges = function() {
       $('.'+settings.activeClass).not(settings.allBadgeSelector).each( function() {
+        $(this).removeClass(settings.activeClass).data('active', false)
+        this.filterValue.lastValue = ''
+      })
+    }
+
+    var deactivateAllBadges = function() {
+      $('.'+settings.activeClass).each( function() {
         $(this).removeClass(settings.activeClass).data('active', false)
         this.filterValue.lastValue = ''
       })
